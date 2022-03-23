4efa8861771987e361cf75be24fef1e604d43788
Jonathan Yeminy <jonathanyeminy@yahoo.com>
2022-03-22 20:46:34 -0400

    commit tree c0fd1a5c0e00ff03cc7f351493854eb541ece873

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
-const init = () => {
-  
-}
+const init = () => {    
+let formData = document.querySelector("form");
+//console.log(formData)
+formData.addEventListener("submit", e => handleSubmit(e))
 
-document.addEventListener('DOMContentLoaded', init);
+function handleSubmit(e) {
+    e.preventDefault()
+    let input = document.getElementById("searchByID").value
+    //console.log(input)
+    
+    fetch("http://localhost:3000/movies")
+    .then(resp => resp.json())
+    .then(function(data){
+        
+        const bookInfo = data[`${input-1}`]
+        console.log(bookInfo)
+        document.querySelector("h4").innerText = bookInfo.title
+        document.querySelector("p").innerText = bookInfo.summary
+    })
+}
+}
+document.addEventListener('DOMContentLoaded', init)
